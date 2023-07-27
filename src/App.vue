3d73896851baaf60cc76a188c59a41fdc734096b
iqbalzeeee <iqbalnauval920@gmail.com>
2023-07-27 10:47:59 +0700

    latihannn

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <NavbarComponent></NavbarComponent>
+    <CarouselComponent></CarouselComponent>
+    <br>
+    <CardComponent></CardComponent>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script setup>
+import NavbarComponent from './components/NavbarComponent.vue';
+import CarouselComponent from './components/CarouselComponent.vue';
+import CardComponent from './components/CardComponent.vue';
+import { Footer } from 'flowbite-vue';
+</script>
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,166 @@
+<script setup>
+import { TheCard } from 'flowbite-vue'
+import { Button } from 'flowbite-vue'
+import { Table, TableHead, TableBody, TableHeadCell, TableRow, TableCell } from 'flowbite-vue'
+</script>
+<template>
+    <center>
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">NEWS JKT48</h5>
+    </center>
+    <center>
+    <div class="flex flex-row">
+  <div class="basis-1/2">
+    <the-card variant="image" img-src="https://2.bp.blogspot.com/-YYLzN67vOUM/WAzMoSlPttI/AAAAAAAADbM/iCHlXm_uctku2dfHw3pljjqsLKKUcE9NwCLcB/s640/JKT48.jpg" img-alt="Desk">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">THEATER</h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">
+      Pengumuman Mengenai Video Call Pengganti Cornelia Vanisa dan Amanda Sukma
+    </p>
+    <Button color="black">
+    Choose plan
+    <template #suffix>
+      <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+    </template>
+  </Button>
+  </the-card>
+  </div>
+  <div class="basis-1/2">
+    <the-card variant="image" img-src="https://2.bp.blogspot.com/-YYLzN67vOUM/WAzMoSlPttI/AAAAAAAADbM/iCHlXm_uctku2dfHw3pljjqsLKKUcE9NwCLcB/s640/JKT48.jpg" img-alt="Desk">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">OTHER</h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">
+      Pengumuman Mengenai Pre-Order Digital Photobook "JKT48 Summer Festival Diary"
+    </p>
+    <Button color="black">
+    Choose plan
+    <template #suffix>
+      <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+    </template>
+  </Button>
+  </the-card>
+  </div>
+  <div class="basis-1/2">
+    <the-card variant="image" img-src="https://2.bp.blogspot.com/-YYLzN67vOUM/WAzMoSlPttI/AAAAAAAADbM/iCHlXm_uctku2dfHw3pljjqsLKKUcE9NwCLcB/s640/JKT48.jpg" img-alt="Desk">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">THEATER</h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">
+      Pengumuman Mengenai Merchandise Spesial JKT48 Summer Festival
+    </p>
+    <Button color="black">
+    Choose plan
+    <template #suffix>
+      <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+    </template>
+  </Button>
+  </the-card>
+  </div>
+  <the-card variant="image" img-src="https://2.bp.blogspot.com/-YYLzN67vOUM/WAzMoSlPttI/AAAAAAAADbM/iCHlXm_uctku2dfHw3pljjqsLKKUcE9NwCLcB/s640/JKT48.jpg" img-alt="Desk">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">THEATER</h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">
+      Pengumuman Mengenai Merchandise Spesial JKT48 Summer Festival
+    </p>
+    <Button color="black">
+    Choose plan
+    <template #suffix>
+      <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+    </template>
+  </Button>
+  </the-card>
+
+</div>
+</center>
+<br>
+<center>
+<Button color="red" pill outline square>
+    Check All
+    <template #suffix>
+      <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+    </template>
+  </Button>
+</center>
+
+<br><br>
+  <center>
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">ANGGOTA JKT48</h5>
+    </center>
+<br>
+
+<center>
+    <div class="flex flex-row">
+  <div class="basis-1/2">
+    <the-card variant="image" img-src="https://static.showroom-live.com/image/room/cover/80e7ab4bb7445c959ca2bff7c44dac4cc7353bddc0d3c70b6928c00f67ccfc53_l.jpeg?v=1685542774" img-alt="Desk">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Indira Seruni</h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">
+        Member Generasi 10
+    </p>
+  </the-card>
+  </div>
+  <div class="basis-1/2">
+    <the-card variant="image" img-src="https://corsyava.com/wp-content/uploads/2023/02/sr-oniel-2.jpeg" img-alt="Desk">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Cornelia Vanisa</h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">
+        Member Generasi 8
+    </p>
+  </the-card>
+  </div>
+  <div class="basis-1/2">
+    <the-card variant="image" img-src="https://static.showroom-live.com/image/room/cover/a9aa6279cf53c4b3f2d79e35a098e94934506edc56b60896dcc44a45f940611e_l.jpeg?v=1675092985" img-alt="Desk">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Azizi Asadel</h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">
+        Member Generasi 7
+    </p>
+  </the-card>
+  </div>
+  <div class="basis-1/2">
+    <the-card variant="image" img-src="https://static.showroom-live.com/image/room/cover/fc878be06fb484b4edcb8051e8456a93279b2395e711aada3d7547b1b78e3720_l.jpeg?v=1683720193" img-alt="Desk">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Michelle Alexandra</h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">
+        Member Generasi 11
+    </p>
+  </the-card>
+  </div>
+</div>
+</center>
+<br>
+<center>
+<Button color="red" pill outline square>
+    Check All
+    <template #suffix>
+      <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+    </template>
+  </Button>
+</center>
+
+<br><br>
+  <center>
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">JKT48 SCHEADULE</h5>
+    </center>
+<br>
+
+<center>
+  <Table striped>
+    <table-head>
+      <table-head-cell>SETLIST ON GOING</table-head-cell>
+      <table-head-cell>SCHEADULE</table-head-cell>
+    </table-head>
+    <table-body>
+      <table-row>
+        <table-cell>Tunas di Balik Seragam</table-cell>
+        <table-cell>THEATER</table-cell>
+        <table-cell>
+        </table-cell>
+      </table-row>
+      <table-row>
+        <table-cell>Aturan Anti Cinta</table-cell>
+        <table-cell>THEATER</table-cell>
+        <table-cell>
+        </table-cell>
+      </table-row>
+      <table-row>
+        <table-cell>Cara Meminum Ramune</table-cell>
+        <table-cell>THEATER</table-cell>
+        <table-cell>
+        </table-cell>
+      </table-row>
+    </table-body>
+  </Table>
+</center>
+
+</template>
